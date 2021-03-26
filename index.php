e7973fe4ae226e301f2ffd879a633c40415bd10d
Bruno Moreno <brunomorenobm@gmail.com>
2021-03-26 14:51:04 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,78 @@
+ <?php
+
+    /**
+     * Plugin Name: Consultas REST simples e diretas
+     * Plugin URI: https://bake.com.vc
+     * Description: Consultas simples para listagem de produtos de forma fácil e direta.
+     * Author URI: https://bake.com.vc
+     * Text Domain: wp-easy-rest-api
+     * Domain Path: /api
+     * License: GPLv2 or later
+     * Version: 0.1
+     * Text Domain: easy-rest
+     * Requires at least: 5.6
+     * Requires PHP: 5.6
+     */
+
+
+    add_action('rest_api_init', function () {
+        register_rest_route('easy-rest/v1', '/products/(?P<id>\d+)', array(
+            'methods' => 'GET',
+            'callback' => 'get_products_easilly',
+            'permission_callback' => 'permission_callback',
+        ));
+    });
+
+    function permission_callback($data)
+    {
+        return true;
+    }
+
+
+
+    function get_products_easilly($data)
+    {
+        $dbProducts = wc_get_products(array('status' => 'publish', 'limit' => -1));
+        $jsonProducts = array();
+        header('Content-Type: application/json; charset=UTF-8');
+
+        if (empty($dbProducts)) {
+            return null;
+        }
+
+        $product_data[] = [];
+        $i = 0;
+        // Get an instance of the WC_Order object
+        foreach ($dbProducts as $product) {
+            $product_data[$i]['product_id']       = $product->get_id();
+            $product_data[$i]['category_id']     = $product->get_category_ids('view')[0];
+            $product_data[$i]['name']         = $product->get_name();
+            $product_data[$i]['description'] = $product->get_description();
+            $product_data[$i]['price']  = $product->get_price();
+            $product_data[$i]['price']  = $product->get_regular_price();
+            if ($product->get_type() == 'variable') {
+                $product_data[$i]['variations']  = get_product_variations($product->get_available_variations());
+            }
+            $product_data[$i]['type'] = $product->get_type();
+            $product_data[$i]['status'] = $product->get_status();
+
+            // $product_data[$i]['data'] = $product->get_data();
+            $i++;
+        }
+        return  $product_data;
+    }
+
+    function get_product_variations($variations)
+    {
+        $variation_data[] = [];
+        $i = 0;
+        foreach ($variations as $variation) {
+            $variation_data[$i]['variation_id'] = $variation['variation_id'];
+            $variation_data[$i]['price'] = $variation['display_price'];
+            $variation_data[$i]['variation_text'] = array_values($variation['attributes'])[0];
+            $i++;
+        }
+        return $variation_data;
+    }
+
+    ?>
